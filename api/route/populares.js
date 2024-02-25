4a52c6754e2bbf5a93e20e271beeed0e4a1462d9
Jonathan Kirch <jonathankirch101@gmail.com>
2024-02-25 16:14:57 -0300

    modificar rota populares para enviar mais informaçãos sobre os livros

--- api/route/populares.js
+++ api/route/populares.js
@@ -6,27 +6,43 @@ const router = express.Router();
 const apiURL = `https://www.googleapis.com/books/v1/volumes`;
 
 router.get('/populares', async (req, res) => {
-  try {
-    const response = await axios.get(
-      `${apiURL}?q=''&orderBy=relevance`
-    );
-    const livros = response.data.items.map((item) => ({
-      titulo: item.volumeInfo.title,
-      autor: item.volumeInfo.authors,
-      imagem:
-        item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.thumbnail
-          ? item.volumeInfo.imageLinks.thumbnail || item.volumeInfo.imageLink.smallThumbnail
-          : 'https://placehold.co/500?text=Livro+sem+capa',
-      descricao: item.volumeInfo.description ?? 'Sem descrição'
-    }));
+	try {
+		const response = await axios.get(`${apiURL}?q=''&orderBy=relevance`);
 
-    res.json({
-      livros,
-    });
-  } catch (err) {
-    console.error(`Erro na API: ${err}`);
-    res.status(500).send(`Erro ao buscar livros`);
-  }
+		const livros = response.data.items.map((item) => ({
+			titulo: item.volumeInfo.title,
+			autor: item.volumeInfo.authors
+				? item.volumeInfo.authors.join(', ')
+				: 'Autor desconhecido',
+			descricao: item.volumeInfo.description || 'Sem descrição',
+			imagem:
+				item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.thumbnail
+					? item.volumeInfo.imageLinks.thumbnail ||
+					  item.volumeInfo.imageLinks.smallThumbnail
+					: 'https://placehold.co/500?text=Livro+sem+capa',
+			numeroPaginas: item.volumeInfo.pageCount || 'Informação não disponível',
+			dataPublicacao:
+				item.volumeInfo.publishedDate || 'Data idisponível',
+			editora: item.volumeInfo.publisher || 'Editora não disponível',
+			valor:
+				item.saleInfo &&
+				item.saleInfo.listPrice &&
+				item.saleInfo.listPrice.amount
+					? item.saleInfo.listPrice.amount
+					: 'Preço não disponível',
+			linkCompra:
+				item.saleInfo && item.saleInfo.buyLink
+					? item.saleInfo.buyLink
+					: 'Link de compra não disponível',
+		}));
+
+		res.json({
+			livros,
+		});
+	} catch (err) {
+		console.error(`Erro na API: ${err}`);
+		res.status(500).send(`Erro ao buscar livros`);
+	}
 });
 
 module.exports = router;
